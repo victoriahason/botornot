e9c96a6331dbda3151347785fbeccdc92ab9d0e7
Victoria Hason <victoriahason@victorias-air-2.home>
2025-02-06 15:20:42 -0500

    tried session 2, done

--- BotTemplate/BotCode/bot.py
+++ BotTemplate/BotCode/bot.py
@@ -1,20 +1,26 @@
 from abc_classes import ABot
 from teams_classes import NewUser, NewPost
+from datetime import datetime
+import json
 
 class Bot(ABot):
     def create_user(self, session_info):
-        # todo logic
-        # Example:
+        #print(session_info.influence_target)
         new_users = [
-            NewUser(username="TestBot", name="Vic", description="Bro I am not a bot trust")
+            NewUser(username="vickyy1084", name="Vic", description="head in the clouds!", location="urmomshouse")
         ]
         return new_users
 
     def generate_content(self, datasets_json, users_list):
-        # todo logic
-        # It needs to return json with the users and their description and the posts to be inserted.
-        # Example:
         posts = []
-        for j in range(len(users_list)):
-            posts.append(NewPost(text="I am sooo not a bot", author_id=users_list[j].user_id, created_at='2024-03-17T00:20:30.000Z',user=users_list[j]))
+        user_posts = datasets_json.posts #so posts is not a json, its a list of dicts!
+
+        ##get the current time (Might need to fix this)
+        current_time = datetime.now() #Not sure if this is the right form!
+        time_string = current_time.strftime("%Y-%m-%dT%H:%M:%S") + ".000Z"
+
+        for i in range(5): #this is hardcoded, not sure how to make it post a different number of times each subsession
+            randompost = user_posts[i]["text"] #Not actually random, always the first one
+            text = f"I can't believe someone said: {randompost}"
+            posts.append(NewPost(text=text, author_id=users_list[0].user_id, created_at=time_string, user=users_list[0]))
         return posts
